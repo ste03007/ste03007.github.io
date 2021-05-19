b6767c41e7397dc0a6b7b33a89d8461cb1b30046
ste03007 <ste03007.byui.edu>
2021-05-18 18:01:49 -0600

    date

--- lesson4/js/preston index.js
+++ lesson4/js/preston index.js
@@ -13,29 +13,4 @@ window.onresize = () => {
   if (window.innerWidth > 760) {
     mainMenu.classList.remove("responsive");
   }
-};
-
-const daynames = [
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-];
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-const todaysdate = new Date();
-const dayName = daynames[todaysdate.getDay()];
-const monthName = months[todaysdate.getMonth()];
-const year = todaysdate.getFullYear();
-const currentDate = dayName + ", " + todaysdate.getDate() + " " + monthName + " " + year;
-const options = {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'};
-document.getElementById("currentDate").textContent = currentDate;
+}
