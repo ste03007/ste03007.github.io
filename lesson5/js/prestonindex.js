333e49538b445448f2a4d2c067a455c945a43464
ste03007 <ste03007.byui.edu>
2021-05-24 00:29:35 -0600

    sss

--- lesson5/js/prestonindex.js
+++ lesson5/js/prestonindex.js
@@ -50,18 +50,3 @@ const options = {
   year: "numeric",
 };
 document.getElementById("currentDate").textContent = currentDate;
-
-
-const div = document.querySelector('div');
-      let winWidth = window.innerWidth;
-      let winHeight = window.innerHeight;
-
-      div.style.width = winWidth + 'px';
-      div.style.height = winHeight + 'px';
-
-      window.onresize = function() {
-        winWidth = window.innerWidth;
-        winHeight = window.innerHeight;
-        div.style.width = winWidth + 'px';
-        div.style.height = winHeight + 'px';
-      }
