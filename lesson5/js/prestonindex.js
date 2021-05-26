84fc3d16a4139b98571ae92f770e4d61586953e8
ste03007 <ste03007.byui.edu>
2021-05-26 17:09:52 -0600

    df

--- lesson5/js/prestonindex.js
+++ lesson5/js/prestonindex.js
@@ -51,6 +51,7 @@ const options = {
 };
 
 document.getElementById("currentDate").textContent = currentDate;
+
 if (thedate.getDay() == 5) {
     document.querySelector("prestonPancakes").style.display = 'block';
 }
