f85dc2726381235653cfc34661db26f2154eabfa
ste03007 <ste03007.byui.edu>
2021-06-03 04:41:40 -0600

    ss

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,30 +1,15 @@
-
-let temperature = parseFloat(document.getElementById("temperature").textContent);
-let windspeed = parseFloat(document.getElementById("windSpeed").textContent);
-
-let windchill = "N/A";
-if (temperature <= 50 && windspeed > 3) {
-    windchill = windChill(temperature, windspeed) + "&deg;F";
-}
-
-document.getElementById("windChill").innerHTML = windchill;
-
-function windChill(tempF, speed) {
-    windchill = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, .16)) + (0.4275 * tempF * Math.pow(speed, .16));
-    return windchill.toFixed(0);
-}
-
-let temperature = parseFloat(document.getElementById("temperature").textContent);
-let windspeed = parseFloat(document.getElementById("windSpeed").textContent);
-
-let windchill = "N/A";
-if (temperature <= 50 && windspeed > 3) {
-    windchill = windChill(temperature, windspeed) + "&deg;F";
-}
-
-document.getElementById("windChill").innerHTML = windchill;
-
-function windChill(tempF, speed) {
-    windchill = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, .16)) + (0.4275 * tempF * Math.pow(speed, .16));
-    return windchill.toFixed(0);
-}
+function windChill(tempF, windSpeed) {
+    let output =
+      35.74 +
+      0.6215 * tempF -
+      35.75 * Math.pow(windSpeed, 0.16) +
+      0.4275 * tempF * Math.pow(windSpeed, 0.16);
+  
+    document.querySelector("#windchill").innerHTML =
+      output.toFixed(1) + "&#176;F";
+  }
+  
+  let wind_chill =
+    tempF <= 50 && windSpeed >= 3
+      ? windChill(tempF, windSpeed)
+      : (document.querySelector("#windchill").innerHTML = "N/A");
