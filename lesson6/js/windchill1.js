7ae7fdf76a409efc614d39522cd78636abefdb5b
ste03007 <ste03007.byui.edu>
2021-06-03 05:41:48 -0600

    ddd

---
+++ lesson6/js/windchill1.js
@@ -0,0 +1,19 @@
+
+let tempF = document.querySelector(".Tempf").innerHTML;
+let windSpeed = document.querySelector(".Windspeed").innerHTML;
+
+function windChill(tempF, windSpeed) {
+  let output =
+    35.74 +
+    0.6215 * tempF -
+    35.75 * Math.pow(windSpeed, 0.16) +
+    0.4275 * tempF * Math.pow(windSpeed, 0.16);
+
+  document.querySelector("#windchill").innerHTML =
+    output.toFixed(1) + "&#176;F";
+}
+
+let wind_chill =
+  tempF <= 50 && windSpeed >= 3
+    ? windChill(tempF, windSpeed)
+    : (document.querySelector("#windchill").innerHTML = "N/A");
