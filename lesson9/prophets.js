5517a7a1593838562aa2c3a02ba0e32bb525eb01
ste03007 <ste03007.byui.edu>
2021-07-06 20:15:41 -0600

    d

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -5,17 +5,20 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-
   const prophets = jsonObject['prophets'];
-  for (let i = 0; i < prophets.length; i++ ) {
+  for (let i = 0; i < prophets.length; i++) {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
+    let img = document.createElement('img');
 
     h2.innerHTML = `${prophets[i].name}<span class="purple">${prophets[i].lastname}</span>`;
     image.setAttribute('src', prophets[i].imageurl);
     image.setAttribute('alt',`The offical portrait of  ${prophets[i].name} ${prophets[i].lastname}!`);
+    image.style.boxShadow = '0 0 50px #444';
 
-    card.appendChild(h2);
+
+    card.append(h2);
+    card.append(img);
 
     document.querySelector('div.cards').appendChild(card);
   }
