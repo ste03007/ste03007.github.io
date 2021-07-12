4207e4bd001e729c2561f86a6b68ff17429ba40d
ste03007 <ste03007.byui.edu>
2021-07-12 14:53:29 -0600

    s

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,29 +1,29 @@
-const requestURL =
-  "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
+
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
-  .then(function (Response) {
-    return Response.json();
+  .then(function (response) {
+    return response.json();
   })
+
   .then(function (jsonObject) {
+    //console.table(jsonObject);
     const prophets = jsonObject['prophets'];
-
-    const idaho = prophets.filter(prophet => prophet.birthplace == "Idaho");
-
-     idaho.forEach(prophet => {
-
-      let card = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let img = document.createElement('img');
-
-      h2.innerHTML = `${prophet.name}<span class="purple">${prophet.lastname}</span>`;
-      image.setAttribute('src', `images/${town.photo}`);
-      image.setAttribute('alt',`The offical portrait of  ${prophet.name} ${prophet.lastname}!`);
-
-      card.append(h2);
-      card.append(img);
-
-      document.querySelector("div.cards").append(card);
-    })
-
+    for (let i = 0; i < prophets.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let birthdayPara = document.createElement('p');
+        let birthplacePara = document.createElement('p');
+        let image = document.createElement('img');
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        birthdayPara.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+        birthplacePara.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+        image.setAttribute('src', prophets[i].imageurl);
+        image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + (i + 1));
+        card.appendChild(h2);
+        card.appendChild(birthdayPara);
+        card.appendChild(birthplacePara);
+        card.appendChild(image);
+        document.querySelector('div.cards').appendChild(card);
+    }
   });
