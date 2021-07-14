ad21821b9be221803f61aaec2512ecc0cd598fee
ste03007 <ste03007.byui.edu>
2021-07-14 17:32:41 -0600

    s

--- lesson11/js/weatherapi.js
+++ lesson11/js/weatherapi.js
@@ -14,8 +14,8 @@ fetch(apiURL)
   function windChill(tempF, windSpeed) {
     return  35.74 + 0.6215 * tempF - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * tempF * Math.pow(windSpeed, 0.16);
   }
-  let tempF =  weatherData.main.temp; // in the future this will be replaced with data from a third party - openweathermap
-  let windSpeed = weatherData.wind.speed; // will be replaced as well
+  let tempF =  document.main.temp; // in the future this will be replaced with data from a third party - openweathermap
+  let windSpeed = document.wind.speed; // will be replaced as well
   let wchill = 35.74 + 0.6215 * tempF - 35.75 * windSpeed ** 0.16 + 0.4275 * tempF * windSpeed ** 0.16;
 
   document.querySelector('#windchill').innerHTML = wchill;
