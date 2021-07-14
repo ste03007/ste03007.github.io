463f3976af7c1d3171cbd8bb4d19584c7b1e23cf
ste03007 <ste03007.byui.edu>
2021-07-13 19:44:17 -0600

    ten final needs windchill still

--- lesson10/js/prestonindex.js
+++
@@ -1,6 +0,0 @@
-const requestURL = https:/history.openweathermap.org/data/2.5/aggregated/year?id=5604473&appid={dd99eea42d98bdd2c3700a8b64a66250}
-
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
