0497afbd6cf3009c601e4512b4a537811a1de062
ste03007 <ste03007.byui.edu>
2021-07-21 01:40:53 -0600

    s

--- chamberofcommerce/js/menu.js
+++ chamberofcommerce/js/menu.js
@@ -1,11 +1,19 @@
-consttoggle=document.querySelector(".toggle");
-constmenu=document.querySelector(".menu");
-function closeSubmenu(e) {
-    let isClickInside = menu.contains(e.target);
-    if (!isClickInside && menu.querySelector(".submenu-active")) {
-      menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    }
-    const items = document.querySelectorAll(".item");
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+
+/* Activate Submenu */
 function toggleItem() {
   if (this.classList.contains("submenu-active")) {
     this.classList.remove("submenu-active");
@@ -14,23 +22,23 @@ function toggleItem() {
     this.classList.add("submenu-active");
   } else {
     this.classList.add("submenu-active");
+  }
+}
 
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
 for (let item of items) {
-    if (item.querySelector(".submenu")) {
-      item.addEventListener("click", toggleItem, false);
-      item.addEventListener("keypress", toggleItem, false);
-           }else{
-        functiontoggleMenu()
-            if(menu.classList.contains("active")){
-            menu.classList.remove("active");
-            toggle.querySelector("a").innerHTML="<iclass=’fasfa-bars’></i>";
-        }else{
-            this.classList.add("submenu-active");
-menu.classList.add("active");
-
-
-toggle.querySelector("a").innerHTML="<iclass=’fasfa-times’></i>";
-
-toggle.addEventListener("click",toggleMenu,false);
-  
-  document.addEventListener("click", closeSubmenu, false);}}}}}}
+  if (item.querySelector(".submenu")) {
+    item.addEventListener("click", toggleItem, false);
+  }
+  item.addEventListener("keypress", toggleItem, false);
+}
+document.addEventListener("click", closeSubmenu, false);
