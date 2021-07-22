a9d7b8cb17dd912a20404d451c505fc8cc70940c
ste03007 <ste03007.byui.edu>
2021-07-21 23:44:12 -0600

    df

--- chamberofcommerce/js/directorys.js
+++ chamberofcommerce/js/directorys.js
@@ -1,4 +1,4 @@
-let section = document.getElementById("directorys");
+let section = document.getElementById("directory");
 let requestURL = "https://ste03007.github.io/chamberofcommerce/business.json";
 let request = new XMLHttpRequest();
 request.open("GET", requestURL);
@@ -8,12 +8,12 @@ request.onload = function () {
   let southdec = request.response;
   idahodec(southdec);
   function idahodec(jsonObj) {
-    let directory = jsonObj["business"];
+    let direc = jsonObj["business"];
     for (let i = 0; i < directory.length; i++) {
       if (
-        directory[i].name == "Auto" ||
-        directory[i].name == "Pre" ||
-        directory[i].name == "Ste"
+        direc[i].name == "Auto" ||
+        direc[i].name == "Pre" ||
+        direc[i].name == "Ste"
       ) {
         let article = document.createElement("article");
         let h2 = document.createElement("h2");
@@ -23,8 +23,8 @@ request.onload = function () {
         let paragraph3 = document.createElement("p");
         let paragraph4 = document.createElement("p");
 
-        h2.textContent = directory[i].business_name;
-        paragraph1.textContent = directory[i].address;
+        h2.textContent = direc [i].business_name;
+        paragraph1.textContent = direc[i].address;
         paragraph2.textContent = "Street: " + directory[i].street;
         paragraph3.textContent = "City: " + directory[i].city;
         paragraph4.textContent = "State:" + directory[i].state;
