e81b7ae88c45bc4705ea3ce0b7ba6bb764806f9a
ste03007 <75203351+ste03007@users.noreply.github.com>
2022-04-02 17:35:20 -0600

    dfdsa

--- familes/js/familes.js
+++ familes/js/familes.js
@@ -196,3 +196,12 @@
 //   },
 //   active: begin
 // });
+const hambutton = document.querySelector('.🍔');
+const mainnav = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) {mainnav.classList.remove('responsive')}};
