ab1863f4b3ef313e61789427eade0a9a1af1f5a7
ste03007 <75203351+ste03007@users.noreply.github.com>
2022-07-14 09:48:20 -0600

    Delete familes directory

--- familes/js/familes.js
+++
@@ -1,9 +0,0 @@
-const hambutton = document.querySelector('.🍔');
-const mainnav = document.querySelector('.navigation');
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-
-
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) {mainnav.classList.remove('responsive')}};
